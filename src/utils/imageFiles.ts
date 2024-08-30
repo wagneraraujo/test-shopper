fe9afd7887a50edc820aca1eaff8b7b09da85b64
Wagner Araújo <wagneraraujo15@gmail.com>
2024-08-30 00:21:02 -0300

    feat: convert image for base64

--- src/utils/imageFiles.ts
+++ src/utils/imageFiles.ts
@@ -1,8 +1,7 @@
 import multer from "multer";
 import path from "path";
-
-export const MAX_UP_FILE = 1024 * 1024 * 10; // 10MB
-export const ACCEPTED_FILE_EXTENSIONS = ["png", "jpg", "jpeg", "pdf"];
+import fs from "fs/promises";
+const MAX_UP_FILE = 1024 * 1024 * 10; // 10MB
 const imageStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `./public/`);
@@ -18,12 +17,23 @@ const imageStorage = multer.diskStorage({
 });
 const imageUploud = multer({
   storage: imageStorage,
+
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpg|JPG|PNG|jpeg|JPEG|PDF)$/)) {
       return cb(new Error("Apenas imagens png ou jpg"));
     }
+    if (file.size > MAX_UP_FILE) {
+      return cb(new Error("Tamanho maximo 10MB"));
+    }
     cb(null, true);
   },
 });
+
+export async function convertToBase64(
+  file: Express.Multer.File,
+): Promise<string> {
+  const buffer = await fs.readFile(file.path);
+  return Buffer.from(buffer).toString("base64");
+}
 
 export default imageUploud;
