89097a612a11d98bd7148f6fab8d5510753e09ac
Wagner Araújo <wagneraraujo15@gmail.com>
2024-08-29 23:56:51 -0300

    feat: sucess uploud image in route

--- src/controllers/ReadImageController.ts
+++ src/controllers/ReadImageController.ts
@@ -1,10 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { StatusCodes } from "http-status-codes";
+import { z } from "zod";
 
 const ReadImageController = {
   async ReadImage(req: Request, res: Response) {
-    const image = req.file;
-    console.log("request imgem");
-    res.status(200).json(image);
+    const apiKey = process.env.GEMINI_API_KEY;
+    if (!apiKey) {
+      throw new Error("GEMINI_API_KEY não está definida");
+    }
+    const genAI = new GoogleGenerativeAI(apiKey);
+
+    const image = req?.file;
+    if (!image) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Arquivo obrigatório",
+      });
+    }
+    try {
+      console.log("imagem ====>", image);
+      res.status(StatusCodes.OK).json(image);
+    } catch (err) {
+      console.log("other error", err);
+      res.status(StatusCodes.BAD_REQUEST).json({
+        message: "Algo deu errado",
+      });
+    }
   },
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
+import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { routerReadImage } from "./routes/ReadImageRoutes";
+import imageUploud from "./utils/imageFiles";
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
+app.use(express.static("./public/"));
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.static("public"));
 app.use(bodyParser.json());
-
 //routes
 app.get("/", (req: any, res: any) => {
   res.send("Olá, este é o meu servidor Express!\n");
 });
 
 //read image routes
-app.use("/read", routerReadImage);
+app.use("/read", imageUploud.single("image"), routerReadImage);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}/`);
